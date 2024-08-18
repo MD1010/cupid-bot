3cc257fe8331b68e6bba852840c7b2dd52e8d2e0
Michael Deglin <michael@alignedup.com>
2024-08-18 13:39:35 +0300

    added new headers

--- src/utils/cupidRequest.ts
+++ src/utils/cupidRequest.ts
@@ -27,6 +27,10 @@ export const fetchCupidAPI = async ({
       "Content-Type": "application/json",
       "x-okcupid-platform": "DESKTOP",
       "x-okcupid-version": "1",
+      "x-okcupid-locale": "en",
+      "x-okcupid-device-id": "b2ecd7a9a08ccb9b",
+      "Tracestate":"userid=790828667968987270",
+      "Traceparent":"00-b84a8a814a48e9732c46fe068ead701a-70621fba7b028351-00"
     },
     body: JSON.stringify(data),
   });
